36e331b73dad465d96d03d7fb0bfb02287e0fa6d
Raj <raj03.singh93@gmail.com>
2021-02-03 20:39:18 +0530

    Add project files.

---
+++ Common.Library/Extensions/ListExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Library.Extensions
+{
+    public static class ListExtension
+    {
+        public static DataTable ToDataTable(this IEnumerable<T> data, string valueTypeDefaultColumn = "Value")
+        {
+
+        }
+    }
+}
