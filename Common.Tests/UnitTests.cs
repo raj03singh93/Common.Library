4e1ee7435c744fc683a35e9fcae70bc97a5b40c4
Raj <raj03.singh93@gmail.com>
2022-01-01 16:21:27 +0530

    Added JwtTokenGenerator

--- Common.Tests/UnitTests.cs
+++ Common.Tests/UnitTests.cs
@@ -1,12 +1,36 @@
+using Common.Model;
+using Common.Utils;
+using System.Collections.Generic;
+using System.Security.Claims;
 using Xunit;
 
 namespace Common.Tests
 {
+    /// <summary>
+    /// Library Unit Test
+    /// </summary>
     public class UnitTests
     {
         [Fact]
-        public void Test1()
+        public void JwtGenerator_Success()
         {
+            // Arrange
+            JwtTokenGeneratorModel jwtTokenGeneratorModel = new()
+            {
+                Issuer = "test",
+                Audiance = "Test",
+                Key = "HowStrongThisKeyShouldbe",
+                ExpireAfter = 5,
+                Claims = new List<Claim>()
+            };
+
+            jwtTokenGeneratorModel.Claims.Add(new Claim(ClaimTypes.Name, "Raj"));
+
+            // Act
+            string token = JwtTokenGenerator.GenerateJwtToken(jwtTokenGeneratorModel);
+
+            // Assert
+            Assert.IsType<string>(token);
 
         }
     }
---
+++ Common/Utils/JwtTokenGenerator.cs
@@ -0,0 +1,38 @@
+﻿using Common.Model;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace Common.Utils
+{
+    public static class JwtTokenGenerator
+    {
+        public static string GenerateJwtToken(JwtTokenGeneratorModel jwtTokenGeneratorModel)
+        {
+            string token = string.Empty;
+            try
+            {
+                var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtTokenGeneratorModel.Key));
+                var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+                var securityToken = new JwtSecurityToken(
+                    jwtTokenGeneratorModel.Issuer,
+                    jwtTokenGeneratorModel.Audiance,
+                    jwtTokenGeneratorModel.Claims,
+                    jwtTokenGeneratorModel.NotBefore,
+                    DateTime.UtcNow.AddMinutes(jwtTokenGeneratorModel.ExpireAfter),
+                    credentials
+                    );
+
+                token = new JwtSecurityTokenHandler().WriteToken(securityToken);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return token;
+        }
+    }
+}
